1f099c75ddb0728bd023f487e064bd5af740a346
javier <javabla19@gmail.com>
2026-06-07 23:21:30 -0500

    Arreglar configuracion Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-import NovaModa from '../components/novamoda-colombia';
+import dynamic from 'next/dynamic'
+
+const NovaModa = dynamic(
+  () => import('../components/novamoda-colombia'),
+  { ssr: false }
+)
+
 export default function Home() {
-  return <NovaModa />;
+  return <NovaModa />
 }
